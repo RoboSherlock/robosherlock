3a8d902820573972885c4c61520e1223a5aa527f
Ferenc Balint-Benczedi <balintbe@cs.uni-bremen.de>
2016-05-10 13:56:46 +0200

    moved finding AE files from run to core/utils/common.h, making it useable from dependent packages

--- src/core/include/rs/utils/common.h
+++ src/core/include/rs/utils/common.h
@@ -1,10 +1,21 @@
 
 //header file for common
 
-#define COLOR_SIZE 12
-namespace rs {
+#include <ros/package.h>
+#include <iostream>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <errno.h>
 
-namespace common{
+
+namespace rs
+{
+
+#define COLOR_SIZE 12
+#define SEARCHPATH "/descriptors/analysis_engines/"
+
+namespace common
+{
 static const uint32_t colors[] = {0xFF0000, /*red*/
                                   0x00FF00, /*lime*/
                                   0x0000FF, /*blue*/
@@ -18,5 +29,45 @@ static const uint32_t colors[] = {0xFF0000, /*red*/
                                   0x800080, /**/
                                   0x008080  /**/
                                  };
+
+void getAEPaths(const std::string ae, std::string &aePath)
+{
+  std::vector<std::string> searchPaths;
+  searchPaths.push_back("");
+  //add core package path
+  searchPaths.push_back(ros::package::getPath("robosherlock") + std::string(SEARCHPATH));
+
+  //look for packages dependent on core and find their full path
+  std::vector<std::string> child_packages;
+  ros::package::command("depends-on robosherlock", child_packages);
+  for(int i = 0; i < child_packages.size(); ++i)
+  {
+    searchPaths.push_back(ros::package::getPath(child_packages[i]) + std::string(SEARCHPATH));
+  }
+
+  struct stat fileStat;
+  for(size_t j = 0; j < searchPaths.size(); ++j)
+  {
+    const std::string file = searchPaths[j] + ae;
+    const std::string fileXML = file + ".xml";
+    //AEs are accepted with or without the xml extension
+    if(!stat(file.c_str(), &fileStat) && S_ISREG(fileStat.st_mode))
+    {
+      aePath = file;
+      break;
+    }
+    else if(!stat(fileXML.c_str(), &fileStat) && S_ISREG(fileStat.st_mode))
+    {
+      aePath = fileXML;
+      break;
+    }
+  }
+  if(aePath.empty())
+  {
+    outInfo("no AE found with that name");
+  }
 }
-}
+
+}//end common namespace
+
+}//end rs namespace
