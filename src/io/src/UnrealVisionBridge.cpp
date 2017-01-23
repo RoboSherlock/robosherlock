d4950437a47ec0b4d7039e6768be02a895710695
Ferenc Balint-Benczedi <balintbe@cs.uni-bremen.de>
2017-01-23 10:42:23 +0100

    check for FC16 in Unreal

--- src/io/src/UnrealVisionBridge.cpp
+++ src/io/src/UnrealVisionBridge.cpp
@@ -67,11 +67,13 @@ void UnrealVisionBridge::readConfig(const boost::property_tree::ptree &pt)
 
 void UnrealVisionBridge::convertDepth(const uint16_t *in, __m128 *out) const
 {
+  #ifdef __F16C__
   const size_t size = (packet.header.width * packet.header.height) / 4;
   for(size_t i = 0; i < size; ++i, in += 4, ++out)
   {
     *out = _mm_cvtph_ps(_mm_set_epi16(0, 0, 0, 0, *(in + 3), *(in + 2), *(in + 1), *(in + 0)));
   }
+  #endif
 }
 
 void UnrealVisionBridge::connectToServer()
