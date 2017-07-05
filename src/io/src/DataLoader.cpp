2558edeee575876ad2476cd78ffac73f470b7335
anindex <eeit2015_an.lt@student.vgu.edu.vn>
2017-07-05 21:35:08 +0700

    Change AE name, auto find pcd file, option to loop

--- src/io/src/DataLoader.cpp
+++ src/io/src/DataLoader.cpp
@@ -7,6 +7,9 @@
 #include <pcl/io/pcd_io.h>
 #include<pcl/io/ply_io.h>
 
+#include <boost/filesystem/operations.hpp>
+#include <boost/filesystem/path.hpp>
+#include <boost/progress.hpp>
 
 #include <rs/types/all_types.h>
 //RS
@@ -17,36 +20,66 @@
 
 using namespace uima;
 
+namespace fs = boost::filesystem;
 
-class LoadPointCloud : public DrawingAnnotator
+
+class DataLoader : public DrawingAnnotator
 {
 private:
   pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud;
 
-  std::string folder;
-  std::vector<std::string* > filenames;
+  std::string path_to_data;
+  std::vector<std::string> filenames;
+
+  bool isLoop;
+  bool isFile;
 
   int iterator;
 
   double pointSize;
 
 public:
-  LoadPointCloud () : DrawingAnnotator(__func__), pointSize(1.0) {
+  DataLoader () : DrawingAnnotator(__func__), pointSize(1.0) {
     cloud = pcl::PointCloud<pcl::PointXYZRGBA>::Ptr(new pcl::PointCloud<pcl::PointXYZRGBA>);
     iterator = 0;
+    isFile = true;
   }
 
   TyErrorId initialize(AnnotatorContext &ctx)
   {
     outInfo("initialize");
-    if(ctx.isParameterDefined("folder"))
+
+    //get parameters
+    if(ctx.isParameterDefined("path_to_data"))
     {
-      ctx.extractValue("folder", folder);
+      ctx.extractValue("path_to_data", path_to_data);
     }
-    if(ctx.isParameterDefined("filenames"))
+    if(ctx.isParameterDefined("isLoop"))
     {
-      ctx.extractValue("filenames", filenames);
+      ctx.extractValue("isLoop", isLoop);
     }
+
+    fs::path full_path(fs::initial_path<fs::path>());
+    full_path = fs::system_complete(fs::path(path_to_data));
+
+    //check if path exists
+    if(!fs::exists(full_path)){
+      outError("Could not found path provided! Please check again");
+    }
+
+    //if it is a directory, find all relevant pcd file
+    if(fs::is_directory(full_path)){
+      fs::directory_iterator end_iter;
+      for(fs::directory_iterator dir_it(full_path); dir_it != end_iter; dir_it++){
+        std::string currEntry = dir_it->path().string();
+        size_t isPCD = currEntry.find(".pcd");
+        if(isPCD != std::string::npos)
+          filenames.push_back(currEntry);
+      }
+
+      isFile = false;
+    }
+
     return UIMA_ERR_NONE;
   }
 
@@ -65,7 +98,21 @@ public:
 
     pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud_ptr (new pcl::PointCloud<pcl::PointXYZRGBA>);
 
-    std::string path = folder + *filenames[iterator];
+    std::string path;
+    if(!isFile){
+      path = filenames[iterator];
+
+      iterator++;
+      if(iterator > filenames.size() - 1){
+        if(isLoop)
+          iterator = 0;
+        else
+          iterator = filenames.size() - 1;
+      }
+    }
+    else{
+      path = path_to_data;
+    }
 
     if(pcl::io::loadPCDFile (path, *cloud_ptr) == -1){
       outError("Could not load point cloud file as PCD type. Check path again!");
@@ -73,10 +120,6 @@ public:
 
     cloud = cloud_ptr;
     outInfo("Cloud size: " << cloud->points.size());
-
-    iterator++;
-    if(iterator > filenames.size() - 1)
-      iterator = 0;
 
     cas.set(VIEW_CLOUD, *cloud);
 
@@ -99,4 +142,4 @@ public:
 };
 
 // This macro exports an entry point that is used to create the annotator.
-MAKE_AE(LoadPointCloud)
+MAKE_AE(DataLoader)
