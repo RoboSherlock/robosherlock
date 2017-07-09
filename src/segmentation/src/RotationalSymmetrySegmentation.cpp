5729c33ad0b5f35cacb8a145ccd2adaf9b20ecf6
anindex <eeit2015_an.lt@student.vgu.edu.vn>
2017-07-09 10:43:06 +0700

    Initialize Rotational Symmetries Segmentation AE

---
+++ src/segmentation/src/RotationalSymmetrySegmentation.cpp
@@ -0,0 +1,127 @@
+#include <uima/api.hpp>
+#include <vector>
+
+//PCL include
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/features/boundary.h>
+#include <pcl/common/io.h>
+#include <pcl/search/impl/kdtree.hpp>
+
+//RS
+#include <rs/scene_cas.h>
+#include <rs/utils/time.h>
+#include <rs/DrawingAnnotator.h>
+#include <rs/types/all_types.h>
+
+#include <rs/segmentation/array_utils.hpp>
+#include <rs/segmentation/BoundarySegmentation.hpp>
+#include <rs/segmentation/RotationalSymmetry.hpp>
+#include <rs/segmentation/RotationalSymmetryScoring.hpp>
+
+#include <rs/occupancy_map/DistanceMap.hpp>
+
+
+
+using namespace uima;
+
+
+class RotationalSymmetrySegmentation : public DrawingAnnotator
+{
+private:
+  pcl::PointCloud<pcl::PointXYZRGBA>::Ptr sceneCloud;
+  pcl::PointCloud<pcl::Normal>::Ptr sceneNormals;
+
+  boost::shared_ptr< DistanceMap<pcl::PointXYZRGBA> > dist_map;
+  Eigen::Vector4f boundingPlane;
+
+  std::vector<RotationalSymmetry> symmetries;
+  int numSymmetries;
+
+  std::vector< float > symmetryScores;
+  std::vector< float > occlusionScores;
+  std::vector< float > smoothnessScores;
+
+  std::vector< std::vector< float > > pointSymScores;
+  std::vector< std::vector< float > > pointOcclusionScores;
+  std::vector< std::vector< float > > pointPerpendicularScores;
+
+  std::vector< std::vector< float > > fgWeights;
+  std::vector< std::vector< float > > bgWeights;
+
+  float differentAngleThreshold;
+  float radiusSearch;
+
+  double pointSize;
+
+public:
+  RotationalSymmetrySegmentation () : DrawingAnnotator(__func__), pointSize(1.0) {
+    sceneCloud = pcl::PointCloud<pcl::PointXYZRGBA>::Ptr(new pcl::PointCloud<pcl::PointXYZRGBA>);
+    sceneNormals = pcl::PointCloud<pcl::Normal>::Ptr(new pcl::PointCloud<pcl::Normal>);
+  }
+
+  TyErrorId initialize(AnnotatorContext &ctx)
+  {
+    outInfo("initialize");
+    return UIMA_ERR_NONE;
+  }
+
+  TyErrorId destroy()
+  {
+    outInfo("destroy");
+    return UIMA_ERR_NONE;
+  }
+
+  TyErrorId processWithLock(CAS &tcas, ResultSpecification const &res_spec)
+  {
+    MEASURE_TIME;
+
+    outInfo("process begins");
+    rs::SceneCas cas(tcas);
+
+    //get RGB cloud
+    cas.get(VIEW_CLOUD, *sceneCloud);
+
+    //get normal cloud
+    cas.get(VIEW_NORMALS, *sceneNormals);
+
+    //get Rotational Symmteries
+    std::vector<rs::RotationalSymmetry> casSymmetries;
+    cas.get(VIEW_ROTATIONAL_SYMMETRIES, casSymmetries);
+    numSymmetries = casSymmetries.size();
+    symmetries.resize(numSymmetries);
+
+    for(size_t it = 0; it < casSymmetries.size(); it++){
+      Eigen::Vector3f currOrigin(casSymmetries[it].origin().x(), casSymmetries[it].origin().y(), casSymmetries[it].origin().z());
+      Eigen::Vector3f currOrientation(casSymmetries[it].orientation().x(), casSymmetries[it].orientation().y(), casSymmetries[it].orientation().z());
+      symmetries[it] = RotationalSymmetry(currOrigin, currOrientation);
+    }
+
+    //discard color information
+    //pcl::PointCloud<pcl::PointXYZ>::Ptr cloudxyz (new  pcl::PointCloud<pcl::PointXYZ>);
+    //pcl::copyPointCloud(*cloud_ptr, *cloudxyz);
+
+    //main execution
+
+
+
+    return UIMA_ERR_NONE;
+  }
+
+  void fillVisualizerWithLock(pcl::visualization::PCLVisualizer& visualizer, const bool firstRun)
+  {
+    const std::string cloudname = this->name + "_cloud";
+
+    if(firstRun){
+      visualizer.addPointCloud(sceneCloud, cloudname);
+      visualizer.setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, pointSize, cloudname);
+    }
+    else{
+      visualizer.updatePointCloud(sceneCloud, cloudname);
+      visualizer.getPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, pointSize, cloudname);
+    }
+  }
+};
+
+// This macro exports an entry point that is used to create the annotator.
+MAKE_AE(RotationalSymmetrySegmentation)
