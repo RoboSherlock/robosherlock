bbbadbc8d99e59a1769879c02b7564ce100792a5
anindex <eeit2015_an.lt@student.vgu.edu.vn>
2017-07-10 20:19:19 +0700

    Implement Point Cloud map that constructs connectivity graph for foreground segmentation

---
+++ src/segmentation/include/rs/occupancy_map/PointCloudMap.hpp
@@ -0,0 +1,51 @@
+#ifndef POINT_CLOUD_MAP_HPP
+#define POINT_CLOUD_MAP_HPP
+
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/search/kdtree.h>
+
+#include <rs/utils/output.h>
+
+#include <rs/graph/GraphBase.hpp>
+#include <rs/graph/GraphPrimitives.hpp>
+
+template<typename PointT, typename VertexT, typename EdgeT>
+inline bool computeCloudMap(typename pcl::PointCloud<PointT>::Ptr& cloud,
+                            std::vector<int>& indices,
+                            GraphBase<VertexT, EdgeT>& graph,
+                            const float radius,
+                            const int numNeighbors = 0)
+{
+  if(radius <= 0.0f){
+    outError("Radius search must be positive!");
+    return false;
+  }
+
+  graph.setVertices(cloud->points.size());
+
+  pcl::search::KdTree<PointT>& tree;
+  tree.setInputCloud(cloud, boost::make_shared< std::vector<int> >(indices));
+
+  for(size_t pointIdIt = 0; pointIdIt < indices.size(); pointIdIt++){
+    int pointId = indices[pointIdIt];
+
+    std::vector<int> neighborIndices;
+    std::vector<float> dists;
+
+    tree.radiusSearch(pointIdIt, radius, neighborIndices, dists, numNeighbors);
+
+    for(size_t it = 0; it < neighborIndices.size();it++){
+      if(pointId != neighborIndices[it])
+        graph.addEdge(pointId, neighborIndices[it]);
+    }
+  }
+  
+  if(graph.getNumEdges() < 1){
+    outInfo("Point cloud map has no edges!");
+    return false;
+  }
+  return true;
+}
+
+#endif
