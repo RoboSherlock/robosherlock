be857d63b9d034852f7d843b684e18ad4f3e6957
cowe <cowe@tzi.de>
2017-07-26 16:00:11 +0200

    Set up unit testing with coverage

---
+++ test/robosherlock/test.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <gtest/gtest.h>
+#include "rs/utils/RSAnalysisEngineManager.h"
+#include "rs/utils/RSAnalysisEngine.h"
+#include <rs/utils/common.h>
+#include <rs/types/all_types.h>
+#include <rs/scene_cas.h>
+
+#include <pcl/point_types.h>
+#include <ros/ros.h>
+
+class ExampleTest : public ::testing::Test
+{
+protected:
+    virtual void SetUp()
+    {
+        rs::common::getAEPaths("test",engineFile);
+        engine.init(engineFile);
+    }
+
+    virtual void TearDown(){}
+    std::string engineFile;
+    RSAnalysisEngine engine;
+
+};
+
+
+TEST_F(ExampleTest, ProcessTest){
+
+  engine.process();
+  uima::CAS* tcas = engine.getCas();
+  rs::SceneCas cas(*tcas);
+  pcl::PointCloud<pcl::Normal>::Ptr normal_ptr(new pcl::PointCloud<pcl::Normal>);
+
+  cas.get(VIEW_NORMALS, *normal_ptr);
+  EXPECT_TRUE(normal_ptr->points.size()>0);
+}
