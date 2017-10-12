b88be1c5e6270e7a583280e3bd0a167400107489
Ferenc Balint-Benczedi <balintbe@cs.uni-bremen.de>
2017-10-12 17:50:44 +0200

    started migration

--- src/utils/include/rs/utils/DepthImageProcessing.h
+++ src/utils/include/rs/utils/DepthImageProcessing.h
@@ -24,8 +24,8 @@
 #include <pcl/point_types.h>
 #include <opencv2/opencv.hpp>
 #include <image_geometry/pinhole_camera_model.h>
-#include <opencv2/gpu/gpu.hpp>
-#include <opencv2/gpu/gpumat.hpp>
+//#include <opencv2/gpu/gpu.hpp>
+//#include <opencv2/gpu/gpumat.hpp>
 
 namespace rs
 {
