53eff50dcfdf2c86ce913a3e27805c2b58b2237b
Constantin <cowe@tzi.de>
2018-05-14 17:05:00 +0200

    PrologQueries was moved to another pacakge

--- src/flowcontrol/include/rs/flowcontrol/RSProcessManager.h
+++ src/flowcontrol/include/rs/flowcontrol/RSProcessManager.h
@@ -73,7 +73,7 @@ public:
   bool jsonQueryCallback(robosherlock_msgs::RSQueryService::Request &req,
                          robosherlock_msgs::RSQueryService::Response &res);
 
-  virtual bool handleQuery(std::string& req, std::vector<std::string>& res);
+  bool handleQuery(std::string& req, std::vector<std::string>& res);
 #endif
   //special case for offscreen rendering the beliefstate using Unreal Engine
   bool renderOffscreen(std::string object);
