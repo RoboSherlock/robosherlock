00957256845696a3af877206bac3426baf414c16
anindex <eeit2015_an.lt@student.vgu.edu.vn>
2018-05-18 09:19:36 +0700

    Prepare classes

---
+++ src/flowcontrol/include/rs/flowcontrol/RSParallelPipelinePlanner.h
@@ -0,0 +1,24 @@
+/**
+ * Copyright 2014 University of Bremen, Institute for Artificial Intelligence
+ * Author(s): Patrick Mania <pmania@cs.uni-bremen.de>
+ *         Ferenc Balint-Benczedi <balintbe@cs.uni-bremen.de>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef RSPARALLEL_PIPELINE_PLANNER_H
+#define RSPARALLEL_PIPELINE_PLANNER_H
+
+
+
+#endif
