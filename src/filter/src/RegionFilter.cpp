60c131aca6e9c44bd57fc1462bf4e23904f4f84e
anindex <eeit2015_an.lt@student.vgu.edu.vn>
2018-06-27 14:36:30 +0700

    Why filter cloud with emptyindices (only called reserve), this cause exceptions in PlaneAnnotator, NormalEstimator so temporary remove it

--- src/filter/src/RegionFilter.cpp
+++ src/filter/src/RegionFilter.cpp
@@ -151,13 +151,13 @@ private:
     rs::SceneCas cas(tcas);
     rs::Scene scene = cas.getScene();
 
-    cas.get(VIEW_CLOUD, *cloud);
+    //cas.get(VIEW_CLOUD, *cloud);
     cas.get(VIEW_COLOR_IMAGE, color);
     cas.get(VIEW_DEPTH_IMAGE, depth);
     cas.get(VIEW_CAMERA_INFO, cameraInfo);
 
-    indices->clear();
-    indices->reserve(cloud->points.size());
+    //indices->clear();
+    //indices->reserve(cloud->points.size());
 
     camToWorld.setIdentity();
     if(scene.viewPoint.has())
@@ -255,12 +255,12 @@ private:
       }
     }
 
-    pcl::ExtractIndices<PointT> ei;
+    /*pcl::ExtractIndices<PointT> ei;
     ei.setKeepOrganized(true);
     ei.setIndices(indices);
     ei.filterDirectly(cloud);
 
-    cas.set(VIEW_CLOUD, *cloud);
+    cas.set(VIEW_CLOUD, *cloud);*/
 
     if(changeDetection && !indices->empty())
     {
