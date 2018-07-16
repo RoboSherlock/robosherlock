60a17b14faccfb1fd9da99eed8f10d0f77c9cd1b
MirrorIce <ciubotarubred@gmail.com>
2018-07-16 10:54:58 +0200

    Personal unit test added, needs to be edited

---
+++ test/robosherlock_utest/test_utest.cpp
@@ -0,0 +1,60 @@
+#include <string>
+#include <gtest/gtest.h>
+#include "rs/flowcontrol/RSAnalysisEngineManager.h"
+#include "rs/flowcontrol/RSPipelineManager.h"
+
+#include "rs/flowcontrol/RSAnalysisEngine.h"
+#include <rs/utils/common.h>
+#include <rs/types/all_types.h>
+#include <rs/scene_cas.h>
+
+#include <pcl/point_types.h>
+#include <ros/ros.h>
+
+#include <mongo/client/dbclient.h>
+
+class ExampleTest : public testing::Test
+{
+protected:
+    virtual void SetUp()
+    {
+      rs::common::getAEPaths("u_test",engineFile);
+      engine.init(engineFile);
+      engine.initPipelineManager();
+    }
+
+    virtual void TearDown()
+    {
+       engine.stop();
+    }
+    mongo::client::GlobalInstance instance;
+    std::string engineFile;
+    RSAnalysisEngine engine;
+};
+
+TEST_F(ExampleTest, FirstUnitTest)
+{
+  
+  std::vector<std::string> engineList = {"CollectionReader"};
+  outInfo(engineList);
+  engine.process(); 
+
+  
+  uima::CAS* tcas = engine.getCas();
+  rs::SceneCas cas(*tcas);
+  pcl::PointCloud<pcl::Normal>::Ptr normal_ptr(new pcl::PointCloud<pcl::Normal>);
+  cas.get(VIEW_NORMALS, *normal_ptr);
+  EXPECT_TRUE(5>4);
+}
+
+/*TEST_F(ExampleTest, PlaneEstimatorTest)
+{
+  std::vector<std::string> engineList = {"CollectionReader","PlaneAnnotator"};
+  engine.getPipelineManager()->setPipelineOrdering(engineList);
+  engine.process();
+  rs::SceneCas cas(*engine.getCas());
+  rs::Scene scene = cas.getScene();
+  std::vector< rs::Plane > planes;
+  scene.annotations.filter(planes);
+  EXPECT_TRUE(planes.size() >0);
+}*/
