59a62065d1b18b243e12d91893555a97367d40f7
MirrorIce <12020876+MirrorIce@users.noreply.github.com>
2018-08-14 16:48:20 +0200

    ClustColorHistogramCalculator UTest.

--- test/robosherlock_utest/PointCloudClusterExtractor_utest.cpp
+++ test/robosherlock_utest/PointCloudClusterExtractor_utest.cpp
@@ -1,40 +1,7 @@
 
-#include <string.h>
 #include <gtest/gtest.h>
-#include <errno.h>
-#include <sys/stat.h>
-#include <thread>
-#include <mutex>
-#include <chrono>
-#include <condition_variable>
-
-#include <pcl/point_types.h>
-#include <pcl/io/pcd_io.h>
-#include <pcl/surface/convex_hull.h>
-#include <pcl/common/centroid.h>
-#include <pcl/segmentation/extract_polygonal_prism_data.h>
-#include <pcl/segmentation/extract_clusters.h>
-#include <pcl/filters/extract_indices.h>
-#include <pcl/filters/filter.h>
-#include <pcl/segmentation/euclidean_cluster_comparator.h>
-#include <pcl/segmentation/organized_connected_component_segmentation.h>
-#include <pcl/search/kdtree.h>
-#include <pcl/search/impl/kdtree.hpp>
-
-#include <uima/fsfilterbuilder.hpp>
-
-#include <ros/ros.h>
-#include <rs/flowcontrol/RSProcessManager.h>
-#include <rs/flowcontrol/RSAnalysisEngineManager.h>
-#include <rs/utils/common.h>
-#include <rs/io/Storage.h>
-
-#include <rs/conversion/conversion.h>
-
-#include <ros/ros.h>
-#include <ros/package.h>
 #include <rs/scene_cas.h>
-
+#include <rs/flowcontrol/RSAnalysisEngine.h>
 #undef OUT_LEVEL
 #define OUT_LEVEL OUT_LEVEL_DEBUG
 #include "../main.h"
@@ -58,7 +25,6 @@ void processCluster(uima::CAS *cas)
     EXPECT_TRUE(roi.width>0);
     EXPECT_TRUE(roi.height>0);
   }
-  cv::Rect roi;
 }
 
 void pointCloudExtractorTest()
