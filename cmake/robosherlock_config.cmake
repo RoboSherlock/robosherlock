78938275961036f73196611e8b0e0332a5163eaf
Alexander Link <link@uni-bremen.de>
2018-11-23 10:39:14 +0100

    Revert "Fixes after merge that included adding other required packages into this package."
    This reverts commit 77a53a7ea0f0586233287586e793c4d4c859816a.

--- cmake/robosherlock_config.cmake
+++
@@ -1,6 +0,0 @@
-set(robosherlock_NAMESPACE           rs)
-set(robosherlock_TYPESYSTEM_CPP_PATH /home/alexander/tracking/src/robosherlock/src/core/include/rs/types)
-set(robosherlock_TYPESYSTEM_XML_PATH /home/alexander/tracking/src/robosherlock/descriptors/typesystem)
-set(robosherlock_ANNOTATOR_PATH      /home/alexander/tracking/src/robosherlock/descriptors/annotators)
-set(robosherlock_ENGINE_PATH         /home/alexander/tracking/src/robosherlock/descriptors/analysis_engines)
-
