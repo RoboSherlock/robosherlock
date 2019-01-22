e480e070f5c3dfee4e075a76e8ac2b45f23dbe52
Alexander Link <link@uni-bremen.de>
2019-01-22 15:43:48 +0100

    Base for new ClosestHypothesisFilter to figure out which object we're looking for, depending on where it was in the previous tracking iteration.

---
+++ robosherlock/src/filter/src/ClosestHypothesisFilter.cpp
@@ -0,0 +1,48 @@
+#include <uima/api.hpp>
+
+#include <pcl/point_types.h>
+#include <rs/types/all_types.h>
+//RS
+#include <rs/scene_cas.h>
+#include <rs/utils/time.h>
+
+using namespace uima;
+
+
+class ClosestHypothesisFilter : public Annotator
+{
+private:
+  float test_param;
+
+public:
+
+  TyErrorId initialize(AnnotatorContext &ctx)
+  {
+    outInfo("initialize");
+    ctx.extractValue("test_param", test_param);
+    return UIMA_ERR_NONE;
+  }
+
+  TyErrorId destroy()
+  {
+    outInfo("destroy");
+    return UIMA_ERR_NONE;
+  }
+
+  TyErrorId process(CAS &tcas, ResultSpecification const &res_spec)
+  {
+    outInfo("process start");
+    rs::StopWatch clock;
+    rs::SceneCas cas(tcas);
+    pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud_ptr(new pcl::PointCloud<pcl::PointXYZRGBA>);
+    outInfo("Test param =  " << test_param);
+    cas.get(VIEW_CLOUD,*cloud_ptr);
+
+    outInfo("Cloud size: " << cloud_ptr->points.size());
+    outInfo("took: " << clock.getTime() << " ms.");
+    return UIMA_ERR_NONE;
+  }
+};
+
+// This macro exports an entry point that is used to create the annotator.
+MAKE_AE(ClosestHypothesisFilter)
