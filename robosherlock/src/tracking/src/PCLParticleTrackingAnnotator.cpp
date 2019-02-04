0fe0c19ba3a842b626cdbf2c6aacc14bbb4ccfd5
Unknown <link@uni-bremen.de>
2019-02-04 12:34:10 +0100

    white background color for PCL visualizer

--- robosherlock/src/tracking/src/PCLParticleTrackingAnnotator.cpp
+++ robosherlock/src/tracking/src/PCLParticleTrackingAnnotator.cpp
@@ -268,6 +268,7 @@ public:
       {
         visualizer.addPointCloud(particle_cloud, red_color, cloudname);
         visualizer.setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, pointSize, cloudname);
+        visualizer.setBackgroundColor (255, 255, 255);
       }
       else
       {
