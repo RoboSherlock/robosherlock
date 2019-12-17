22207f2ecebb55afcb49ebcd59ec9aa9bf4b322c
Sanic <Sanic@users.noreply.github.com>
2019-12-17 10:06:59 +0100

    added test file for visualizables

---
+++ robosherlock/test/robosherlock/test_visualizables.cpp
@@ -0,0 +1,58 @@
+#include <string>
+#include <gtest/gtest.h>
+
+#include <rs/utils/common.h>
+#include <rs/types/all_types.h>
+#include <rs/scene_cas.h>
+#include <rs/io/Visualizable.h>
+
+#include <pcl/point_types.h>
+#include <ros/ros.h>
+
+#include <iostream>
+
+class TestVisualizable : public Visualizable
+{
+public:
+  TestVisualizable(const std::string& name) : Visualizable(name)
+  {
+    outInfo("Instantiation of TestVisualizable");
+  }
+};
+
+class VisualizablesTest : public testing::Test
+{
+protected:
+
+
+  VisualizablesTest()
+  {
+
+  }
+  virtual void SetUp()
+  {
+    Visualizable::clearVisualizableList();
+  }
+
+  virtual void TearDown()
+  {
+
+  }
+};
+
+//q_subClassOf
+TEST_F(VisualizablesTest, Instantiation)
+{
+  TestVisualizable tv1("Visualizable1");
+  TestVisualizable tv2("Visualizable2");
+
+  std::map<std::string, Visualizable*> mapOfVisualizables;
+  Visualizable::copyVisualizableList(mapOfVisualizables);
+  outInfo("Size of map: " << mapOfVisualizables.size());
+  EXPECT_TRUE(mapOfVisualizables.size() == 2);
+
+  Visualizable::clearVisualizableList();
+  Visualizable::copyVisualizableList(mapOfVisualizables);
+  EXPECT_TRUE(mapOfVisualizables.size() == 0);
+}
+
