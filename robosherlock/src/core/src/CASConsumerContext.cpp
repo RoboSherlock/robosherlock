736b2266e0febb116b690f59c682e3c2bbda14bb
Sanic <sanic@users.noreply.github.com>
2020-01-08 16:16:00 +0100

    cas consumer context draft

---
+++ robosherlock/src/core/src/CASConsumerContext.cpp
@@ -0,0 +1,33 @@
+//
+// Created by pmania on 08.01.20.
+//
+
+#include "rs/CASConsumerContext.h"
+
+using namespace rs;
+
+// Add a new CAS that should be accessible for the CAS Consumers
+void CASConsumerContext::addCAS(std::string identifier, uima::CAS* cas)
+{
+  CASes[identifier] = cas;
+}
+// Remove a CAS by its identifier. Returns true if key was presented
+bool CASConsumerContext::removeCAS(std::string identifier)
+{
+  // return true if more than 0 elements have been deleted.
+  return CASes.erase(identifier) > 0;
+}
+// Get CAS by the identifier you've used in addCAS. Returns nullptr if identifier couldn't be found.
+uima::CAS* CASConsumerContext::getCAS(std::string identifier)
+{
+  if(CASes.count(identifier) > 0){
+    return CASes.at(identifier);
+  }
+
+  return nullptr;
+}
+// Remove all stored CAS pointers in this CASConsumerContext
+void CASConsumerContext::clearCASes()
+{
+  CASes.clear();
+}
