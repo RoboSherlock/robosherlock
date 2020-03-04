6dfb8ad18cf225633284e7d97ad7a87a86cd0c64
Ferenc Balint-Benczedi <balintbe@cs.uni-bremen.de>
2020-03-04 16:14:48 +0100

    use only local pl definitions; even when using rosprolog

--- robosherlock/prolog/init.pl
+++ robosherlock/prolog/init.pl
@@ -1,16 +1,23 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % dependencies
-:- register_ros_package(knowrob_common).
-:- register_ros_package(knowrob_objects).
-:- register_ros_package(knowrob_vis).
 :- register_ros_package(robosherlock).
 
-:- owl_parser:owl_parse('package://iai_kitchen/owl/iai-kitchen-objects.owl').
-:- rdf_db:rdf_register_prefix(kitchen, 'http://knowrob.org/kb/iai-kitchen.owl#', [keep(true)]).
+:- use_module(library(semweb/rdfs)). % reasoning about queries
+:- use_module(library(semweb/rdf_db)). % reasoning about queries
+:- use_module(external/owl).
+:- use_module(external/rdfs).
+:- use_module(external/owl_2).
 
-:- owl_parser:owl_parse('package://robosherlock/owl/rs_components.owl').
+:- rdf_db:rdf_register_ns(rdfs,    'http://www.w3.org/2000/01/rdf-schema#',     [keep(true)]).
+:- rdf_db:rdf_register_ns(owl,     'http://www.w3.org/2002/07/owl#',            [keep(true)]).
+:- rdf_db:rdf_register_ns(rdf, 'http://www.w3.org/1999/02/22-rdf-syntax-ns#', [keep(true)]).
+
+:- rdf_db:rdf_register_prefix(kitchen, 'http://knowrob.org/kb/iai-kitchen.owl#', [keep(true)]).
 :- rdf_db:rdf_register_prefix(rs_components, 'http://knowrob.org/kb/rs_components.owl#', [keep(true)]).
 
+:- rdf_load('../owl/rs_components.owl').
+:- rdf_load('../owl/iai-kitchen-objects.owl').
 
-:- use_module(library(rs_interface)). % a ROS-prolog interface for interacting with RobOSherlock
-:- use_module(library(rs_query_reasoning)). % reasoning about queries
+:- use_module(rs_query_reasoning). % reasoning about queries
+
+
