755bb7db434542f1084ac0b71fd78af6d88e4a6a
Evan Kapitzke <evan@uni-bremen.de>
2020-04-28 23:51:19 +0200

    Prolog CPPQueries: reconfigure_annotator, overwrite_param

--- robosherlock/src/queryanswering/src/PrologCPPQueries.cpp
+++ robosherlock/src/queryanswering/src/PrologCPPQueries.cpp
@@ -5,6 +5,8 @@
 
 #include "robosherlock_msgs/RSQueryService.h"
 #include "robosherlock_msgs/ExecutePipeline.h"
+#include "robosherlock_msgs/ReconfigureAnnotator.h"
+#include "robosherlock_msgs/OverwriteParam.h"
 
 #include <ros/package.h>
 
@@ -199,4 +201,124 @@ PREDICATE(write_list, 1)
     std::cout << (char *)e << std::endl;
   }
   return TRUE;
+}
+
+
+/**
+ * Conversion methods, needed for overwrite_param service calls
+ */
+
+std::vector<int> getIntVector(PlTail prologList)
+{
+  std::vector<int> result;
+  PlTerm e;
+
+  while(prologList.next(e)) {
+    result.push_back(e);
+  }
+
+  return result;
+}
+
+// ROS FLOAT = DOUBLE!
+std::vector<double> getFloatVector(PlTail prologList)
+{
+  std::vector<double> result;
+  PlTerm e;
+
+  while(prologList.next(e)) {
+    result.push_back(*(double *)&e);
+  }
+
+  return result;
+}
+
+std::vector<std::string> getStringVector(PlTail prologList)
+{
+  std::vector<std::string> result;
+  PlTerm e;
+
+  while(prologList.next(e)) {
+    result.push_back(static_cast<std::string>((char *)e));
+  }
+  return result;
+}
+
+
+PREDICATE(cpp_reconfigure_annotator, 2)
+{
+  std::string *annotatorName, *setupName;
+  annotatorName = static_cast<std::string *>((void *) PL_A1);
+  setupName = static_cast<std::string *>((void *) PL_A2);
+
+  ros::NodeHandle n;
+  ros::ServiceClient client = n.serviceClient<robosherlock_msgs::ReconfigureAnnotator>("RoboSherlock/reconfigure_annotator");
+  robosherlock_msgs::ReconfigureAnnotator srv;
+
+  srv.request.annotatorName = *annotatorName;
+  srv.request.setupName = *setupName;
+
+  if(client.call(srv)) {
+    std::cout << "Calling RoboSherlock/reconfigure_annotator was successful" << std::endl;
+    return TRUE;
+  }
+  else {
+    std::cout << "Calling RoboSherlock/reconfigure_annotator was unsuccessful" << std::endl;
+    return FALSE;
+  }
+}
+
+
+PREDICATE(cpp_overwrite_param, 4)
+{
+  std::string *annotatorName, *paramName;
+  annotatorName = static_cast<std::string *>((void *) PL_A1);
+  paramName = static_cast<std::string *>((void *) PL_A2);
+  int paramType = PL_A3;
+  void *value = PL_A4;
+
+  ros::NodeHandle n;
+  ros::ServiceClient client = n.serviceClient<robosherlock_msgs::OverwriteParam>("RoboSherlock/overwrite_param");
+  robosherlock_msgs::OverwriteParam srv;
+
+  srv.request.annotatorName = *annotatorName;
+  srv.request.parameterName = *paramName;
+  srv.request.parameterType = paramType;
+
+  switch(paramType) {
+    case robosherlock_msgs::OverwriteParam::Request::INT:
+      srv.request.parameterInteger = PL_A4;
+      break;
+    case robosherlock_msgs::OverwriteParam::Request::INT_VECTOR:
+      srv.request.parameterIntegerVector = getIntVector(PL_A4);
+      break;
+    case robosherlock_msgs::OverwriteParam::Request::BOOL:
+      srv.request.parameterBool = *(bool *)value;
+      break;
+    case robosherlock_msgs::OverwriteParam::Request::FLOAT:
+      srv.request.parameterFloat = PL_A4;
+      break;
+    case robosherlock_msgs::OverwriteParam::Request::FLOAT_VECTOR:
+      srv.request.parameterFloatVector = getFloatVector(PL_A4);
+      break;
+    case robosherlock_msgs::OverwriteParam::Request::STRING:
+      srv.request.parameterString = *(std::string *)value;
+      break;
+    case robosherlock_msgs::OverwriteParam::Request::STRING_VECTOR:
+      srv.request.parameterStringVector = getStringVector(PL_A4);
+      break;
+
+    default:
+      std::cout << "Invalid parameter data type, aborting." << std::endl;
+      return FALSE;
+  }
+
+  if(client.call(srv)) {
+    std::cout << "Calling RoboSherlock/overwrite_param was successful" << std::endl;
+    return TRUE;
+  }
+  else {
+    std::cout << "Calling RoboSherlock/overwrite_param was unsuccessful" << std::endl;
+    return FALSE;
+  }
 }
