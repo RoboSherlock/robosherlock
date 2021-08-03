66086608b2b16918490f60a69137621194d2a6ab
sunava <hassouna@cs.uni-bremen.de>
2021-08-03 16:21:33 +0200

    new pkg for filter algos

---
+++ robosherlock_filter/cmake/robosherlock_filter_config.cmake
@@ -0,0 +1,6 @@
+set(robosherlock_filter_NAMESPACE           robosherlock_filter)
+set(robosherlock_filter_TYPESYSTEM_CPP_PATH /home/hassouna/rs_ws/src/robosherlock/robosherlock_filter/include/robosherlock_filter/types)
+set(robosherlock_filter_TYPESYSTEM_XML_PATH /home/hassouna/rs_ws/src/robosherlock/robosherlock_filter/descriptors/typesystem)
+set(robosherlock_filter_ANNOTATOR_PATH      /home/hassouna/rs_ws/src/robosherlock/robosherlock_filter/descriptors/annotators)
+set(robosherlock_filter_ENGINE_PATH         /home/hassouna/rs_ws/src/robosherlock/robosherlock_filter/descriptors/analysis_engines)
+
---
+++ robosherlock_filter/cmake/rs_filter_config.cmake
@@ -0,0 +1,6 @@
+set(rs_filter_NAMESPACE           robosherlock_filter)
+set(rs_filter_TYPESYSTEM_CPP_PATH /home/hassouna/rs_ws/src/robosherlock/robosherlock_filter/include/robosherlock_filter/types)
+set(rs_filter_TYPESYSTEM_XML_PATH /home/hassouna/rs_ws/src/robosherlock/robosherlock_filter/descriptors/typesystem)
+set(rs_filter_ANNOTATOR_PATH      /home/hassouna/rs_ws/src/robosherlock/robosherlock_filter/descriptors/annotators)
+set(rs_filter_ENGINE_PATH         /home/hassouna/rs_ws/src/robosherlock/robosherlock_filter/descriptors/analysis_engines)
+
