9741ea52f838f2a5b44001955411a48bb24a4bbb
sunava <hassouna@cs.uni-bremen.de>
2021-08-04 15:35:50 +0200

    new subprojects

---
+++ robosherlock_recognition/cmake/robosherlock_recognition_config.cmake
@@ -0,0 +1,6 @@
+set(robosherlock_recognition_NAMESPACE           robosherlock_recognition)
+set(robosherlock_recognition_TYPESYSTEM_CPP_PATH /home/hassouna/rs_ws/src/robosherlock/robosherlock_recognition/include/robosherlock_recognition/types)
+set(robosherlock_recognition_TYPESYSTEM_XML_PATH /home/hassouna/rs_ws/src/robosherlock/robosherlock_recognition/descriptors/typesystem)
+set(robosherlock_recognition_ANNOTATOR_PATH      /home/hassouna/rs_ws/src/robosherlock/robosherlock_recognition/descriptors/annotators)
+set(robosherlock_recognition_ENGINE_PATH         /home/hassouna/rs_ws/src/robosherlock/robosherlock_recognition/descriptors/analysis_engines)
+
