6dbdee95b270a74391f24b34b7d96570be634ce0
AB <55253316+abmuslim@users.noreply.github.com>
2021-11-17 16:00:47 +0100

    Update PointCloudCLusterExtractor.cpp for ROS noetic version

--- robosherlock/src/segmentation/src/PointCloudClusterExtractor.cpp
+++ robosherlock/src/segmentation/src/PointCloudClusterExtractor.cpp
@@ -373,14 +373,15 @@ private:
                                 std::vector<pcl::PointIndices> &cluster_indices,
                                 const pcl::PointIndices::Ptr &prism_inliers)
   {
-      std::vector<bool> ignore_labels;
+      //std::vector<bool> ignore_labels;
     pcl::PointCloud<pcl::Label>::Ptr input_labels(new pcl::PointCloud<pcl::Label>);
 
     pcl::Label label;
-
-    ignore_labels.resize(2);
-    ignore_labels[0] = true;
-    ignore_labels[1] = false;
+    boost::shared_ptr<std::set<uint32_t> > exclude_labels = boost::make_shared<std::set<uint32_t> > ();
+    exclude_labels->insert (1);
+    //ignore_labels.resize(2);
+    //ignore_labels[0] = true;
+    //ignore_labels[1] = false;
 
 
     input_labels->height = cloud->height;
@@ -409,7 +410,7 @@ private:
     ecc->setInputCloud(cloud);
     ecc->setLabels(input_labels);
     //TODO commented that out i think we have to fix the error
-    //ecc->setExcludeLabels(ignore_labels);
+    ecc->setExcludeLabels(exclude_labels);
     ecc->setDistanceThreshold(cluster_tolerance, true);
     ecc->setInputNormals(normals);
     std::vector<pcl::PointIndices> cluster_i;
